0db925c4798ed6ae2840bde5726e3f3406d5ac9b
Ayu Sudi <ayusudi.abc@gmail.com>
2023-02-05 10:53:20 +0700

    Rename footer

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,7 +1,7 @@
 </div>
 <footer class="bg-white sticky-footer">
     <div class="container my-auto">
-        <div class="text-center my-auto copyright"><span>Copyright © Brand 2023</span></div>
+        <div class="text-center my-auto copyright"><span>Copyright © TK4 2023</span></div>
     </div>
 </footer>
 </div>
